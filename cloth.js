10754493097c0eac8fb6badabb9a465117af33b2
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-01-19 20:09:35 -0700

    a lot

--- cloth.js
+++ cloth.js
@@ -1,3 +1,30 @@
 console.log("Injected Into Roblox")
 var step1 = window.location.href.split('catalog/').pop()
 var assetId = step1.split("/")[0]
+
+function ParseXML(data) {
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(data, "text/xml");
+    var roblox = xmlDoc.getElementsByTagName("roblox")[0]
+    var Item = roblox.getElementsByTagName("Item")[0]
+    var Properties = Item.getElementsByTagName("Properties")[0]
+    var Content = Properties.getElementsByTagName("Content")[0]
+    var url = Content.getElementsByTagName("url")[0]
+    return url.childNodes[0].nodeValue
+}
+
+async function getAssetJson(url) {
+    // Json Response
+    const JSONresponse = await fetch(url)
+    var JSONdata = await JSONresponse.json()
+    var JsonLocation = JSONdata["location"]
+    // console.log(JsonLocation)
+    // XML Response
+    const XMLresponse = await fetch(JsonLocation)
+    var XMLdata = await XMLresponse.text()
+    // Parse XML
+    var parXML = ParseXML(XMLdata)
+    console.log(parXML)
+}
+
+getAssetJson("https://assetdelivery.roblox.com/v1/assetId/" + assetId)
