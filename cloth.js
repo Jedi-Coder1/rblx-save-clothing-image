54c4feb095b84e54a5875443dc058171d2f26b03
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-01-21 09:37:00 -0700

    rename

--- cloth.js
+++ cloth.js
@@ -48,7 +48,7 @@ async function getAssetJson(asset) {
             var dropdown = popover.getElementsByClassName("dropdown-menu")[0]
             var li = dropdown.getElementsByTagName("li")[0]
             var libraryBtn = document.createElement("a")
-            var text = document.createTextNode("Open Library Page")
+            var text = document.createTextNode("Open Image In New Tab")
             libraryBtn.appendChild(text)
             libraryBtn.setAttribute("href", imgSrc)
             libraryBtn.setAttribute("target", "_blank")
